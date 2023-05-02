33ddaceea7e68f3bae95fc5cfc0ea3191bb6bb6c
Jaspreet Khela <jaspreetkhela@Jaspreets-MBP.ht.home>
2023-05-02 19:02:46 -0400

    Finished creating the database models using SQLAlchemy and seeding the database

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # python-newsfeed
+
+Left off on lesson 2
+"The engine variable manages the overall connection to the database." bullet point
--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,7 @@ def create_app(test_config=None):
     app.url_map.strict_slashes = False
     app.config.from_mapping(SECRET_KEY='super_secret_key')
     
-    @app.route('/hello')
-    
+    @app.route('/hello')    
     def hello():
         return 'hello world'
     
